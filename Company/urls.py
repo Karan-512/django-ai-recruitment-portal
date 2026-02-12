cc990e9575f0a550e3c34b036f1a159d1f675285
karan-512-2 <karangaikwad009@gmail.com>
2026-02-12 12:27:20 +0530

    Company: 1. Added the base template. 2. Added Files for the other screens.

--- Company/urls.py
+++ Company/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 
     path('home/', views.Home, name='company-home'),
+    path('profile', views.Profile, name='company-profile'),
+    path('job-post/', views.jobPosting, name='job-post'),
+    path('view-applications/', views.ViewApplications, name='applications'),
 ]
--- Company/views.py
+++ Company/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def Home(request):
-    return render(request, 'c-base.html')
+    return render(request, 'dashboard.html', {'pageTitle': "Company Dashboard"})
+
+@login_required
+def Profile(request):
+    return render(request, 'profile.html', {'pageTitle': "Company Profile"})
+
+
+@login_required
+def jobPosting(request):
+    return render(request, 'jobPosting.html', {'pageTitle': "Post a Job"})
+
+@login_required
+def ViewApplications(request):
+    return render(request, 'applications.html', {'pageTitle': "View Applications"})
